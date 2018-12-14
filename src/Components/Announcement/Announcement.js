14df5acd68d50c7eebe96a4cb74382a2a4d81b35
Neha A <42008366+Neha-Anil@users.noreply.github.com>
2018-12-14 20:42:40 +0530

    first commit

--- src/Components/Announcement/Announcement.js
+++ src/Components/Announcement/Announcement.js
@@ -5,8 +5,15 @@ class Announcement extends React.Component{
 	constructor(props) {
     super(props);
 
+
     }
-       
+   onAnnChange=(event)=>{
+    this.setState({message:event.target.value})
+  }    
+  handleMsg=()=>{
+    console.log(document.getElementById('msg').value)
+
+  }
 	render(){
 	return (
     <div>
@@ -14,8 +21,12 @@ class Announcement extends React.Component{
     <div style={{display:'flex',justifyContent:'center',padding:'15px'}} className='shadow-5 pat'>
       <div className='h'>Leave a message!</div>
       <div style={{display:'flex'}}>
-      <input className='f3 pa2 w-80 t' type='text' />
-      <button style={{display:'flex'}} className=' w-20 grow f4 link ph3 pv2 dib white pointer bg-light-purple'><center>Enter</center></button>
+      <input className='f3 pa2 w-80 t'
+      
+       type='text' id="msg" />
+      <button 
+      onClick={this.handleMsg}
+      style={{display:'flex'}} className=' w-20 grow f4 link ph3 pv2 dib white pointer bg-light-purple'><center>Enter</center></button>
       </div>
     </div>
     </div>
